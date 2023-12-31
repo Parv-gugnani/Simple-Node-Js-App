d6fb40950684ca630d1547230a9ded511892c475
Parv-gugnani <9765.parv@gmail.com>
2023-12-31 14:25:31 +0530

    public

--- index.js
+++ index.js
@@ -3,6 +3,19 @@ const mongoose = require("mongoose");
 const port = 80;
 const app = express();
 
+mongoose.connect("mongodb://localhost/projectDG", {
+  useNewUrlPraser: true,
+  useUnifiedTopology: true,
+});
+let db = mongoose.connection;
+
+app.use(express.json());
+
+app.use(exptrss.static("public"));
+app.use(exptrss.urlencoded({ extended: true }));
+
+app.get("/", res);
+
 app.get("/", (req, res) => {
   res.send("Hello, world!");
 });
